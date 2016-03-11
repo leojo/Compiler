40f14099e638857bb2c0e17e30bcc1a8e70da210
leoj <leoj@decode.is>
2016-03-11 12:15:44 +0000

    Changed spaces to tabs (Because yes it is better)

--- Parser.java
+++ Parser.java
@@ -9,24 +9,24 @@ import java.util.HashMap;
  */
 public class Parser {
 
-    private static Lexer lexer;
-    private static Yytoken next_token;
-    private static boolean verbose;
+	private static Lexer lexer;
+	private static Yytoken next_token;
+	private static boolean verbose;
 	private static HashMap<String, Integer> variables;
 	private static int nextVarNum;
 	
-	static public final int NAME     = 0;
-	static public final int VAR      = 1;
+	static public final int NAME	 = 0;
+	static public final int VAR	  = 1;
 	static public final int RETURN   = 2;
 	static public final int OPERATOR = 3;
 	static public final int LITERAL  = 4;
-	static public final int IF       = 5;
-	static public final int ELIF     = 6;
-	static public final int ELSE     = 7;
-	static public final int WHILE    = 8;
+	static public final int IF	   = 5;
+	static public final int ELIF	 = 6;
+	static public final int ELSE	 = 7;
+	static public final int WHILE	= 8;
 
-	static public final int ERR      = -1;
-	static public final int EOF      = -2;
+	static public final int ERR	  = -1;
+	static public final int EOF	  = -2;
 	
 	public Parser(Lexer l, boolean v){
 		lexer = l;
@@ -34,83 +34,77 @@ public class Parser {
 		advance();
 	}
 	
-    public static void advance()
-    {	
-        try
-        {
-            next_token = lexer.yylex();
-        }
-        catch (IOException e)
-        {
-            throw new Error(e);
-        }
-        if( next_token == null )
-        {
-            next_token = new Yytoken(EOF,"EOF");
-        }
-        if( next_token.number == ERR )
-        {
-            System.err.println("Unexpected symbol : "+next_token+" (line: "+(lexer.getLine()+1)+", column: "+lexer.getColumn()+")");
-            System.exit(1);
-        }
-    }
-
-    private static String expectedType(int i){
-        String expectedType;
-        switch (i){
-            case VAR: expectedType = "'var'";
-                break;
-            case NAME: expectedType = "<NAME>";
-                break;
-            case RETURN: expectedType = "'return'";
-                break;
-            case LITERAL: expectedType = "<LITERAL>";
-                break;
-            case WHILE: expectedType = "'while'";
-                break;
-            case OPERATOR: expectedType = "<OPERATOR>";
-                break;
-            case IF: expectedType = "'if'";
-                break;
-            case ELIF: expectedType = "'elsif'";
-                break;
-            case ELSE: expectedType = "'else'";
-                break;
-            case EOF: expectedType = "<EOF>";
-                break;
-            case ERR: expectedType = "<ERR>";
-                break;
-            default: expectedType = "'"+((char)i)+"'";
-                break;
-        }
-        return expectedType;
-    }
-	
-	private static void expected( String exp )
-	{
-		System.err.println("Expected "+exp+", found "+next_token.string+" (line: "+(lexer.getLine()+1)+", column: "+lexer.getColumn()+")");
-        System.exit(1);
+	public static void advance(){	
+		try{
+			next_token = lexer.yylex();
+		}
+		catch (IOException e){
+			throw new Error(e);
+		}
+		if( next_token == null ){
+			next_token = new Yytoken(EOF,"EOF");
+		}
+		if( next_token.number == ERR ){
+			System.err.println("Unexpected symbol : "+next_token+" (line: "+(lexer.getLine()+1)+", column: "+lexer.getColumn()+")");
+			System.exit(1);
+		}
 	}
 
-    private static String expect(int i) {
+	private static String expectedType(int i){
+		String expectedType;
+		switch (i){
+			case VAR: expectedType = "'var'";
+				break;
+			case NAME: expectedType = "<NAME>";
+				break;
+			case RETURN: expectedType = "'return'";
+				break;
+			case LITERAL: expectedType = "<LITERAL>";
+				break;
+			case WHILE: expectedType = "'while'";
+				break;
+			case OPERATOR: expectedType = "<OPERATOR>";
+				break;
+			case IF: expectedType = "'if'";
+				break;
+			case ELIF: expectedType = "'elsif'";
+				break;
+			case ELSE: expectedType = "'else'";
+				break;
+			case EOF: expectedType = "<EOF>";
+				break;
+			case ERR: expectedType = "<ERR>";
+				break;
+			default: expectedType = "'"+((char)i)+"'";
+				break;
+		}
+		return expectedType;
+	}
+	
+	private static void expected( String exp ){
+		System.err.println("Expected "+exp+", found "+next_token.string+" (line: "+(lexer.getLine()+1)+", column: "+lexer.getColumn()+")");
+		System.exit(1);
+	}
+
+	private static String expect(int i) {
 		String lexeme = next_token.toString();
-        if(!look(i)) {
+		if(!look(i)) {
 			String expectedType = expectedType(i);
-            expected(expectedType);
-        }
-        advance();
+			expected(expectedType);
+		}
+		advance();
 		return lexeme;
-    }
+	}
 
-    private static void expect(char c){
-        expect((int)c);
-    }
+	private static void expect(char c){
+		expect((int)c);
+	}
 
-    private static void expect(String s){
-        for(char c : s.toCharArray()){
-            expect(c);
-        }
-    }
+	private static void expect(String s){
+		for(char c : s.toCharArray()){
+			expect(c);
+		}
+	}
 	
 	private static boolean look(int i){
 		return next_token.number == i;
@@ -120,23 +114,23 @@ public class Parser {
 		return look((int)c);
 	}
 
-    private static boolean matches(int a, int... b){
-        for(int i : b){
-            if(a==i){
-                return true;
-            }
-        }
-        return false;
-    }
+	private static boolean matches(int a, int... b){
+		for(int i : b){
+			if(a==i){
+				return true;
+			}
+		}
+		return false;
+	}
 
-    private static void verbose(int indent, String s){
-        if(!verbose) return;
-        String spaces = "";
-        for (int i = 0; i < indent; i++) {
-            spaces += ". ";
-        }
-        System.out.println(spaces+s);
-    }
+	private static void verbose(int indent, String s){
+		if(!verbose) return;
+		String spaces = "";
+		for (int i = 0; i < indent; i++) {
+			spaces += ". ";
+		}
+		System.out.println(spaces+s);
+	}
 	
 	// VARIABLE STORE FUNCTIONS:
 	
@@ -157,20 +151,20 @@ public class Parser {
 		return variables.get(varName);
 	}
 
-    // PARSER STARTS HERE:
+	// PARSER STARTS HERE:
 
-    public static Object[] program(){return program(0);}
+	public static Object[] program(){return program(0);}
 
-    private static Object[] program(int level){
+	private static Object[] program(int level){
 		ArrayList<Object> list = new ArrayList<>();
-        verbose(level, "<program>");
+		verbose(level, "<program>");
 		do{
 			list.add(function(level+1));
 		}while(!look(EOF));
 		return list.toArray();
-    }
+	}
 
-    private static Object[] function(int level){
+	private static Object[] function(int level){
 		ArrayList<Object> list = new ArrayList<>();
 		resetVariableStore();
 		
@@ -201,12 +195,12 @@ public class Parser {
 		}while(!look('}'));
 		expect('}');
 		return list.toArray();
-    }
+	}
 
-    private static int decl(int level){
+	private static int decl(int level){
 		int varCount = 0;
 		verbose(level,"<decl>");
-        expect(VAR);
+		expect(VAR);
 		registerVariable(expect(NAME));
 		varCount++;
 		while(look(',')){
@@ -215,7 +209,7 @@ public class Parser {
 			varCount++;
 		}
 		return varCount;
-    }
+	}
 	
 	private static Object[] expr(int level){
 		ArrayList<Object> list = new ArrayList<>();
@@ -228,15 +222,13 @@ public class Parser {
 		return list.toArray();
 	}
 
-    private static Object[] smallExpr(int level){
+	private static Object[] smallExpr(int level){
 		ArrayList<Object> list = new ArrayList<>();
 		verbose(level,"<smallExpr>");
 		level++;
-		if( look(NAME) )
-		{
+		if( look(NAME) ){
 			String name = expect(NAME);
-			if( look('=') )
-			{
+			if( look('=') ){
 				verbose(level,"<NAME> = <expr>");
 				advance();
 				list.add(Compiler.CodeType.ASSIGN);
@@ -244,7 +236,7 @@ public class Parser {
 				list.add(expr(level+1));
 				return list.toArray();
 			}
-            if( !look('(') ){
+			if( !look('(') ){
 				verbose(level,"<NAME>");
 				list.add(Compiler.CodeType.NAME);
 				list.add(getVarNum(name));
@@ -253,56 +245,49 @@ public class Parser {
 			verbose(level,"<NAME>(<expr>...)");
 			list.add(Compiler.CodeType.CALL);
 			list.add(name);
-            advance();
-            if( look(')') )
-            {
-                advance();
-                return list.toArray();
-            }
-            list.add(expr(level+1));
-            while( look(',') )
-            {
-                advance();
+			advance();
+			if( look(')') ){
+				advance();
+				return list.toArray();
+			}
+			list.add(expr(level+1));
+			while( look(',') ){
+				advance();
 				list.add(expr(level+1));
-            }
-            expect(')');
-            return list.toArray();
+			}
+			expect(')');
+			return list.toArray();
 		}
-		if( look(RETURN) )
-		{
+		if( look(RETURN) ){
 			verbose(level,"return <expr>");
 			list.add(Compiler.CodeType.RETURN);
 			advance();
 			list.add(expr(level+1));
 			return list.toArray();
 		}
-		if( look(OPERATOR) )
-		{
+		if( look(OPERATOR) ){
 			verbose(level,"<OPERATOR> <smallExpr>");
 			list.add(Compiler.CodeType.UNARY);
 			list.add(expect(OPERATOR));
 			list.add(smallExpr(level+1));
 			return list.toArray();
 		}
-		if( look(LITERAL) )
-		{
+		if( look(LITERAL) ){
 			verbose(level,"<LITERAL>");
 			list.add(Compiler.CodeType.LITERAL);
 			list.add(next_token.toString());
 			advance();
 			return list.toArray();
 		}
-		if( look('(') )
-        {
+		if( look('(') ){
 			verbose(level,"(<expr>)");
 			list.add(Compiler.CodeType.PRIORITY);
-            advance();
-            list.add(expr(level+1));
-            expect(')');
-            return list.toArray();
-        }
-		if( look(IF) )
-		{
+			advance();
+			list.add(expr(level+1));
+			expect(')');
+			return list.toArray();
+		}
+		if( look(IF) ){
 			list.add(Compiler.CodeType.IF);
 			verbose(level,"<ifexpr>");
 			expect(IF);
@@ -310,23 +295,20 @@ public class Parser {
 			list.add(expr(level+1));
 			expect(')');
 			list.add(body(level+1));
-			while( look(ELIF) )
-			{
+			while( look(ELIF) ){
 				advance();
 				expect('(');
 				list.add(expr(level+1));
 				expect(')');
 				list.add(body(level+1));
 			}
-			if( look(ELSE) )
-			{
+			if( look(ELSE) ){
 				advance();
 				list.add(body(level+1));
 			}
 			return list.toArray();
 		}
-		if( look(WHILE) )
-		{
+		if( look(WHILE) ){
 			list.add(Compiler.CodeType.WHILE);
 			verbose(level,"<while>");
 			advance();
@@ -338,12 +320,12 @@ public class Parser {
 		}
 		expected("expression");
 		return null;
-    }
+	}
 
-    private static Object[] body(int level){
+	private static Object[] body(int level){
 		ArrayList<Object> list = new ArrayList<>();
 		
-        verbose(level, "<body>");
+		verbose(level, "<body>");
 		expect('{');
 		do{
 			list.add(expr(level+1));
@@ -352,27 +334,26 @@ public class Parser {
 		advance();
 		
 		return list.toArray();
-    }
+	}
 
-    public static void main(String[] args) {
-        try {
-            lexer = new Lexer(new FileReader(args[0]));
-        } catch (FileNotFoundException e) {
-            System.err.println("Could not read file "+args[0]+" , check if path is correct!");
-            System.exit(1);
-        }
+	public static void main(String[] args) {
+		try {
+			lexer = new Lexer(new FileReader(args[0]));
+		} catch (FileNotFoundException e) {
+			System.err.println("Could not read file "+args[0]+" , check if path is correct!");
+			System.exit(1);
+		}
 		
-        verbose = (args.length>1 && args[1].equalsIgnoreCase("-v"));
-        if(args.length>1 && !verbose){
-            System.out.println("Invalid option: "+args[1]+"\nUse '-v' for verbose mode.");
-        }
-        advance();
-        program();
-        if( next_token.number != EOF )
-        {
-            System.err.println("Expected EOF, found " + next_token+" (line: "+(lexer.getLine()+1)+")");
-            System.exit(1);
-        }
-        System.out.println("Program parsed successfully");
-    }
+		verbose = (args.length>1 && args[1].equalsIgnoreCase("-v"));
+		if(args.length>1 && !verbose){
+			System.out.println("Invalid option: "+args[1]+"\nUse '-v' for verbose mode.");
+		}
+		advance();
+		program();
+		if( next_token.number != EOF ){
+			System.err.println("Expected EOF, found " + next_token+" (line: "+(lexer.getLine()+1)+")");
+			System.exit(1);
+		}
+		System.out.println("Program parsed successfully");
+	}
 }
