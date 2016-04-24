8afb8069bb1c4bede6b28b7b4b592ede81aa90cf
leojo <leojohannsson91@gmail.com>
2016-04-24 18:39:41 +0000

    Added yacc.linux for linux users
    linux users no longer need wine to run byacc.exe

--- nedansaekinn/source/build.sh
+++ nedansaekinn/source/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 mkdir -p ../bin
 java -jar jflex-1.6.0.jar nanomorpho.jflex
-./byacc.exe -J -Jclass=Compiler nanoMorpho.byaccj
+./yacc.linux -J -Jclass=Compiler nanoMorpho.byaccj
 javac -d ../bin Lexer.java Compiler.java
 rm *.java
 cp morpho.jar ../bin
